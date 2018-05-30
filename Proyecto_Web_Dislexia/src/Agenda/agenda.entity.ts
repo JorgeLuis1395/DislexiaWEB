81321f58bb159e675d97756019ef52a2e7db3f1d
nchicaiza <38292837+nchicaiza@users.noreply.github.com>
2018-05-30 12:00:04 -0500

    agenda

---
+++ Proyecto_Web_Dislexia/src/Agenda/agenda.entity.ts
@@ -0,0 +1,20 @@
+import {Column, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {UsuarioEntity} from "../Usuario/usuario.entity";
+
+
+export class AgendaEntity {
+
+    @PrimaryGeneratedColumn()
+    id_agenda: number;
+    @Column()
+    fecha_registro: Date;
+
+    @Column()
+    actividad: string;
+
+    @Column()
+    prioridad: string;
+
+    
+
+}
