78576baae9a48dbbe86542912a083728e2c46901
JorgeLuis1395 <carrillojorge1395@hotmail.com>
2018-05-29 21:29:55 -0500

    Juego entity y juego schema

--- Proyecto_Web_Dislexia/src/Juego/juego.entity.ts
+++ Proyecto_Web_Dislexia/src/Juego/juego.entity.ts
@@ -1,21 +1,9 @@
 import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 @Entity('web_dislexia')
-export class UsuarioEntity {
+export class JuegoEntity{
 
     @PrimaryGeneratedColumn()
-    id_usuario: number;
+    id_juego: number;
     @Column({length: 50})
-    nombre: string;
-    @Column({length: 50})
-    apellido: string;
-    @Column({length: 100})
-    correo_electronico: string;
-    @Column()
-    edad: number;
-    @Column({length: 50})
-    usuario: string;
-    @Column({length: 50})
-    contrasenia: string;
-
-
+    dificultad: string;
 }
